9c5cc3322841b2a8c9acd41011ef9e1cfe621a25
Gilson <gilsonjuniorpro@gmail.com>
2020-10-23 19:20:31 -0400

    added fragments, some images and created item shoes layout

---
+++ app/src/main/java/theshoestore/ca/util/Util.kt
@@ -0,0 +1,101 @@
+package theshoestore.ca.util
+
+import theshoestore.ca.R
+import theshoestore.ca.model.Shoes
+
+object Util {
+    fun getListOfShoes(): List<Shoes> {
+        return listOf(
+            Shoes(
+                "New Balance 696v4",
+                "Hit the court in style in the latest update of the 696 -- the New Balance 696v4 Standard D Width in White/Blue. New Balance's newly updated 696v4 tennis shoe offers a grippy herringbone outsole pattern that helps you step your game up on hard courts. This shoe features a synthetic/mesh upper that also features perforations in the forefoot for superior breathability supported by a lightweight midsole.",
+                "$49.00",
+                R.drawable.shoe01
+            ),
+            Shoes(
+                "New Balance 697v4",
+                "Hit the court in style in the latest update of the 696 -- the New Balance 696v4 Standard D Width in White/Blue. New Balance's newly updated 696v4 tennis shoe offers a grippy herringbone outsole pattern that helps you step your game up on hard courts. This shoe features a synthetic/mesh upper that also features perforations in the forefoot for superior breathability supported by a lightweight midsole.",
+                "$79.00",
+                R.drawable.shoe02
+            ),
+            Shoes(
+                "New Balance 698v4",
+                "Hit the court in style in the latest update of the 696 -- the New Balance 696v4 Standard D Width in White/Blue. New Balance's newly updated 696v4 tennis shoe offers a grippy herringbone outsole pattern that helps you step your game up on hard courts. This shoe features a synthetic/mesh upper that also features perforations in the forefoot for superior breathability supported by a lightweight midsole.",
+                "$69.00",
+                R.drawable.shoe03
+            ),
+            Shoes(
+                "New Balance 698v4",
+                "Hit the court in style in the latest update of the 696 -- the New Balance 696v4 Standard D Width in White/Blue. New Balance's newly updated 696v4 tennis shoe offers a grippy herringbone outsole pattern that helps you step your game up on hard courts. This shoe features a synthetic/mesh upper that also features perforations in the forefoot for superior breathability supported by a lightweight midsole.",
+                "$59.00",
+                R.drawable.shoe04
+            ),
+            Shoes(
+                "New Balance 698v4",
+                "Hit the court in style in the latest update of the 696 -- the New Balance 696v4 Standard D Width in White/Blue. New Balance's newly updated 696v4 tennis shoe offers a grippy herringbone outsole pattern that helps you step your game up on hard courts. This shoe features a synthetic/mesh upper that also features perforations in the forefoot for superior breathability supported by a lightweight midsole.",
+                "$89.00",
+                R.drawable.shoe05
+            ),
+            Shoes(
+                "New Balance 696v4",
+                "Hit the court in style in the latest update of the 696 -- the New Balance 696v4 Standard D Width in White/Blue. New Balance's newly updated 696v4 tennis shoe offers a grippy herringbone outsole pattern that helps you step your game up on hard courts. This shoe features a synthetic/mesh upper that also features perforations in the forefoot for superior breathability supported by a lightweight midsole.",
+                "$49.00",
+                R.drawable.shoe01
+            ),
+            Shoes(
+                "New Balance 697v4",
+                "Hit the court in style in the latest update of the 696 -- the New Balance 696v4 Standard D Width in White/Blue. New Balance's newly updated 696v4 tennis shoe offers a grippy herringbone outsole pattern that helps you step your game up on hard courts. This shoe features a synthetic/mesh upper that also features perforations in the forefoot for superior breathability supported by a lightweight midsole.",
+                "$79.00",
+                R.drawable.shoe02
+            ),
+            Shoes(
+                "New Balance 698v4",
+                "Hit the court in style in the latest update of the 696 -- the New Balance 696v4 Standard D Width in White/Blue. New Balance's newly updated 696v4 tennis shoe offers a grippy herringbone outsole pattern that helps you step your game up on hard courts. This shoe features a synthetic/mesh upper that also features perforations in the forefoot for superior breathability supported by a lightweight midsole.",
+                "$69.00",
+                R.drawable.shoe03
+            ),
+            Shoes(
+                "New Balance 698v4",
+                "Hit the court in style in the latest update of the 696 -- the New Balance 696v4 Standard D Width in White/Blue. New Balance's newly updated 696v4 tennis shoe offers a grippy herringbone outsole pattern that helps you step your game up on hard courts. This shoe features a synthetic/mesh upper that also features perforations in the forefoot for superior breathability supported by a lightweight midsole.",
+                "$59.00",
+                R.drawable.shoe04
+            ),
+            Shoes(
+                "New Balance 698v4",
+                "Hit the court in style in the latest update of the 696 -- the New Balance 696v4 Standard D Width in White/Blue. New Balance's newly updated 696v4 tennis shoe offers a grippy herringbone outsole pattern that helps you step your game up on hard courts. This shoe features a synthetic/mesh upper that also features perforations in the forefoot for superior breathability supported by a lightweight midsole.",
+                "$89.00",
+                R.drawable.shoe05
+            ),
+            Shoes(
+                "New Balance 696v4",
+                "Hit the court in style in the latest update of the 696 -- the New Balance 696v4 Standard D Width in White/Blue. New Balance's newly updated 696v4 tennis shoe offers a grippy herringbone outsole pattern that helps you step your game up on hard courts. This shoe features a synthetic/mesh upper that also features perforations in the forefoot for superior breathability supported by a lightweight midsole.",
+                "$49.00",
+                R.drawable.shoe01
+            ),
+            Shoes(
+                "New Balance 697v4",
+                "Hit the court in style in the latest update of the 696 -- the New Balance 696v4 Standard D Width in White/Blue. New Balance's newly updated 696v4 tennis shoe offers a grippy herringbone outsole pattern that helps you step your game up on hard courts. This shoe features a synthetic/mesh upper that also features perforations in the forefoot for superior breathability supported by a lightweight midsole.",
+                "$79.00",
+                R.drawable.shoe02
+            ),
+            Shoes(
+                "New Balance 698v4",
+                "Hit the court in style in the latest update of the 696 -- the New Balance 696v4 Standard D Width in White/Blue. New Balance's newly updated 696v4 tennis shoe offers a grippy herringbone outsole pattern that helps you step your game up on hard courts. This shoe features a synthetic/mesh upper that also features perforations in the forefoot for superior breathability supported by a lightweight midsole.",
+                "$69.00",
+                R.drawable.shoe03
+            ),
+            Shoes(
+                "New Balance 698v4",
+                "Hit the court in style in the latest update of the 696 -- the New Balance 696v4 Standard D Width in White/Blue. New Balance's newly updated 696v4 tennis shoe offers a grippy herringbone outsole pattern that helps you step your game up on hard courts. This shoe features a synthetic/mesh upper that also features perforations in the forefoot for superior breathability supported by a lightweight midsole.",
+                "$59.00",
+                R.drawable.shoe04
+            ),
+            Shoes(
+                "New Balance 698v4",
+                "Hit the court in style in the latest update of the 696 -- the New Balance 696v4 Standard D Width in White/Blue. New Balance's newly updated 696v4 tennis shoe offers a grippy herringbone outsole pattern that helps you step your game up on hard courts. This shoe features a synthetic/mesh upper that also features perforations in the forefoot for superior breathability supported by a lightweight midsole.",
+                "$89.00",
+                R.drawable.shoe05
+            )
+        )
+    }
+}
